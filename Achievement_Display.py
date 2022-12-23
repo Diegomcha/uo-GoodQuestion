b9884a8b342b66ee81c23f9a11ebb54f6954133c
megastacks13 <69910209+megastacks13@users.noreply.github.com>
2022-12-23 17:34:37 +0100

    Achivement Manager Update - Achievement Dsip
    Achievement Manager Update
    Achievement Display appearance
    Data.csv example

---
+++ Achievement_Display.py
@@ -0,0 +1,66 @@
+import Achievement_Manager as am 
+
+TOTAL = 10
+# TOTAL must be a constant since like that we work
+
+# This could be modified if the user wants to so we leave them
+next_key = "C"
+previous_key = "B"
+exit_key = "E"
+
+path = "Data.csv"
+
+def paint(count, total, index, name_from_index, description):
+    """ Makes the display of the achievement
+
+    Args:
+        count (int): total of achievements completed
+        total (int): total of achievements available
+        index (int): index of the achievement
+        name_from_index (String): name of the achievement
+        description (String): description of the achievement
+    """
+    print("----- ACHIEVEMENTS -----")
+    print()
+    print(f"ACHIEVEMENTS ({count} of {total})")
+    print()
+    print(f"{index + 1} - {name_from_index}")
+    print(f"\t{description}")
+    print()
+    print(f"{next_key} - Next")
+    print(f"{previous_key} - Previous")
+    print(f"{exit_key} - Exit")
+    result =  input()
+    
+    if result.upper() == next_key:
+        if index < count -1:
+            paint_logic(index+1)
+        else:
+            paint_logic(index)
+    elif result.upper() == previous_key:
+        if(index > 0):
+            paint_logic(index -1)
+        else:
+            paint_logic(index)
+    elif result.upper == exit_key:
+        pass
+    else:
+        paint_logic(index)
+
+      
+    
+def paint_logic(index_to_paint):
+    """Gets the data for the paint method
+
+    Args:
+        index_to_paint (int): Index of the achievement you want to display
+    """
+    data = am.read_achievements(path)
+    count = len(data)
+    name_from_index = am.separate_achievement(data[index_to_paint])[0]
+    description = am.separate_achievement(data[index_to_paint])[1]
+    
+    paint(count, TOTAL, index_to_paint, name_from_index, description)
+    
+    
+paint_logic(0)
---
+++ Achievement_Manager.py
@@ -0,0 +1,74 @@
+
+def is_in_file(path, name):
+    """Scans through the the file if possible, and returns wether the achievment is in the file or not
+
+    Args:
+        path (dir): Directory of the file you want to read through
+        name (string): The name of the achivement to check
+
+    Returns:
+        True : if in file
+        False : if not in file
+        -1 : ERROR_WRONG_PATH
+    """
+    try:
+        read_file = open(path, "r")
+        data = read_file.readlines()
+        
+        for line in data:
+            if line.count(name) != 0:
+                return True
+            
+        read_file.close()
+        return False
+    except:
+        return -1
+    
+def write_achievement(path, name, description, index):
+    """ Checks if the given achivement is in the file, and in case it is not, it writes
+    
+        Args:
+        path (dir): Directory of the file you want to modify
+        name (string): The name of the achivement to check
+        description (string): The description of the achivement
+
+    Returns:
+        1 : if already in file
+        0 : if written propperly
+        -1 : ERROR_WRONG_PATH
+        """
+    if is_in_file(path, name) == True:
+        return 1
+    elif is_in_file(path,name) == False:
+        write_file = open(path, "a")
+        write_file.write(name + ": "+description+ " -" + index+"\n")
+        write_file.close()
+        return 0
+    else:
+        return -1
+
+def read_achievements(path):
+    """Returns all the values stored in the file 
+
+    Args:
+        path (dir): Directory of the file you want to read through
+
+    Returns:
+        data : list of the values stored in the file
+        -1 : ERROR_WRONG_PATH
+    """
+    try:
+        file = open(path, "r")
+        data = file.readlines()
+        return data
+    except:
+        return -1
+    
+def separate_achievement(achievement_string):
+    result = achievement_string.split(":")
+    return result
+
+
+    
+# Achivements will be saved in a list like so:
+# achivements = [[a1, d1,i1], [a2,d2,i2], [a3,d3,i3], [a4,d4,i4]...]
