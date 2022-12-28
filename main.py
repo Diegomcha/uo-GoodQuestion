42fc088e2c9dcaefbd9a62c99ca97aa361fb38c6
megastacks13 <69910209+megastacks13@users.noreply.github.com>
2022-12-28 18:48:59 +0100

    While loop and don't oversay rooms

--- main.py
+++ main.py
@@ -30,12 +30,14 @@ def main():
         # Displays starting text (lore)
         menu.display_lore()
 
-        # while character['remaining'] > 0:
-        char.display(character)
-        rm.display(character['room'])
-        opt.display(ROOMS[character['room']['id']]['special options'], character)
+        while character['remaining'] > 0:
+            char.display(character)
+            if character['last_room'] != character['room']:
+                rm.display(character['room'])
+            opt.display(ROOMS[character['room']['id']]['special options'], character)
 
         # TODO: Continue...
 
 
 main()
+
